ca2591163bf81659e230527b67dbf05094d67ada
SantiMartinez200 <santimartinez944@gmail.com>
2024-07-17 08:52:29 -0300

    bleach

--- app/Http/Controllers/CajaController.php
+++ app/Http/Controllers/CajaController.php
@@ -6,13 +6,35 @@ use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 use App\Models\Caja;
+use App\Models\MetodoPago;
+use App\Models\User;
 
 class CajaController extends Controller
 {
     public function index(): View
     {
-        $movimientos = Caja::all();
-        return view('caja.index', compact('movimientos'));
+        $cajas = Caja::all();
+        $metodos = MetodoPago::all();
+        $users = User::all();
+        foreach ($cajas as $caja) {
+            if ($caja->metodo_pago_id == $metodos->first()->id) {
+                $caja->metodo_pago_id = $metodos->first()->nombre;
+            } else {
+                $caja->metodo_pago_id = $metodos->find($caja->metodo_pago_id)->nombre;
+            }
+            if($caja->usuario_id == $users->first()->id){
+                $caja->usuario_id = $users->first()->name;
+            }else{
+                $caja->usuario_id = $users->find($caja->usuario_id)->name;
+            }
+            if ($caja->monto_final == 0) {
+                $caja->monto_final = 'N/D';
+            }
+            if($caja->fecha_cierre == null){
+                $caja->fecha_cierre = 'N/D';
+            }
+        }
+        return view('caja.index', compact('cajas','metodos'));
     }
 
     public function create(): View
