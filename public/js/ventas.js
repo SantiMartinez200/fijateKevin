ab8db086b844acc589644506e7aa19026ee51bb3
Santiago Martinez <santimartinez944@gmail.com>
2024-08-01 16:39:29 -0300

    Assets/js/ventas.js,Cantidad&Actualizacion dynamic

---
+++ public/js/ventas.js
@@ -0,0 +1,148 @@
+function fetchURL() {
+    document
+        .getElementById("tbody")
+        .addEventListener("change", function (event) {
+            if (
+                event.target &&
+                event.target.matches('select[name="compra-select[]"]')
+            ) {
+                let id = event.target.value;
+                document
+                    .getElementById("tbody")
+                    .addEventListener("keydown", function (event) {
+                        if (event.key === "-") {
+                            event.preventDefault();
+                        }
+                    });
+                fetch(`/getCompraData/${id}`)
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error(
+                                `Error en la solicitud: ${response.status}`
+                            );
+                        }
+                        return response.json();
+                    })
+                    .then((data) => {
+                        const compraDetails = data.compra;
+                        const currentRow = event.target.closest("tr"); // Encuentra la fila actual
+
+                        const proveedor = currentRow.querySelector(
+                            'input[name="proveedor[]"]'
+                        );
+                        proveedor.value = compraDetails.proveedor_id;
+
+                        const marca = currentRow.querySelector(
+                            'input[name="marca[]"]'
+                        );
+                        marca.value = compraDetails.marca_id;
+
+                        const producto = currentRow.querySelector(
+                            'input[name="producto[]"]'
+                        );
+                        producto.value = compraDetails.producto_id;
+
+                        const aroma = currentRow.querySelector(
+                            'input[name="aroma[]"]'
+                        );
+                        aroma.value = compraDetails.aroma_id;
+
+                        const stock = currentRow.querySelector(
+                            'input[name="stock[]"]'
+                        );
+                        stock.value = compraDetails.cantidad;
+
+                        const precio = currentRow.querySelector(
+                            'input[name="precio[]"]'
+                        );
+                        precio.value = compraDetails.precio_costo;
+
+                        // Cálculos
+                        document
+                            .getElementById("tbody")
+                            .addEventListener("input", function (event) {
+                                if (
+                                    event.target &&
+                                    event.target.matches(
+                                        'input[name="cantidad[]"]'
+                                    )
+                                ) {
+                                    consoelar();
+                                }
+                            });
+                    })
+                    .catch((error) =>
+                        console.error("Error fetching data:", error)
+                    );
+            }
+        });
+}
+fetchURL();
+
+function actualizarTotal() {
+    let totalGeneral = 0;
+    // Recorre todas las filas y suma los totales
+    document.querySelectorAll("#tbody tr").forEach((row) => {
+        const cantidad = row.querySelector('input[name="cantidad[]"]').value;
+        const precio = row.querySelector('input[name="precio[]"]').value;
+        const subtotal = cantidad * precio;
+        totalGeneral += subtotal;
+    });
+    // Muestra el total general
+    if (totalGeneral > 0) {
+        document.getElementById("total-compraLabel").hidden = false;
+        const compraDetailsInput = document.getElementById("total-compra");
+        compraDetailsInput.value = totalGeneral;
+        compraDetailsInput.hidden = false;
+    } else {
+        document.getElementById("total-compraLabel").hidden = true;
+        document.getElementById("total-compra").hidden = true;
+    }
+}
+
+function agregarFila() {
+    // Obtener fila
+    var templateRow = document.querySelector(".template-row");
+    // Clonar fila
+    var newRow = templateRow.cloneNode(true);
+    // Limpiar fila nueva
+    newRow.querySelectorAll("input").forEach((input) => (input.value = ""));
+    newRow
+        .querySelectorAll("select")
+        .forEach((select) => (select.selectedIndex = 0));
+    // Añadir fila a la tabla
+    document.getElementById("tbody").appendChild(newRow);
+}
+
+function eliminarFila(button) {
+    let rowCount = document.querySelectorAll("#tbody tr").length;
+    if (rowCount == 1) {
+        console.log(rowCount);
+        alert("No se puede eliminar la última fila.");
+    } else {
+        // Elimina la fila correspondiente al botón de eliminar
+        button.closest("tr").remove();
+        // actualiza el total
+        actualizarTotal();
+    }
+}
+
+function consoelar() {
+    let rows = document.querySelectorAll("#tbody tr");
+    rows.forEach((row) => {
+        // Obtener inputs de cantidad&stock
+        let cantidadInput = row.querySelector('input[name="cantidad[]"]');
+        let stockInput = row.querySelector('input[name="stock[]"]');
+        cantidadInput.addEventListener("input", function (event) {
+            let cantidad = parseInt(cantidadInput.value);
+            let stock = parseInt(stockInput.value);
+          if (cantidad > stock) {
+              //se remueve el último caracter en caso de que la cantidad ingresada supere al stock.
+                event.target.value = cantidadInput.value.slice(0, -1);
+            } else {
+              actualizarTotal();
+            }
+        });
+    });
+}
+
--- resources/views/ventas/index.blade.php
+++ resources/views/ventas/index.blade.php
@@ -96,11 +96,11 @@
           </tbody>
         </table>
         <div class="form-group">
-          <p id="handle" hidden></p>
-          <button type="button" class="btn btn-primary mr-2" onclick="agregarFila()">Agregar Fila</button>
+          <button type="button" class="btn btn-primary mr-2" onclick="agregarFila()" id="agregar">Agregar Fila</button>
           <input type="submit" value="Guardar" class="btn btn-success" id="guardar">
-          <div class="container" id="handle">
+          <div class="container" id="">
             <div class="d-flex align-items-center mt-2">
+              <p id="handle" class="text text-danger" hidden></p>
               <label class="mt-2" for="total-compra" id="total-compraLabel" hidden>TOTAL: </label>
               <input type="number" name="total-compra" id="total-compra" value=""
                 class="form-control form-control-sm w-25 ml-1" readonly hidden>
@@ -110,93 +110,5 @@
       </form>
     </div>
   </div>
-  <script>
-    function fetchURL() {
-      document.getElementById('tbody').addEventListener('change', function (event) {
-        if (event.target && event.target.matches('select[name="compra-select[]"]')) {
-          let id = event.target.value;
-          document.getElementById('tbody').addEventListener('keydown', function (event) {
-            if (event.key === "-") {
-              event.preventDefault();
-            }
-          })
-          fetch(`/getCompraData/${id}`)
-            .then(response => {
-              if (!response.ok) {
-                throw new Error(`Error en la solicitud: ${response.status}`);
-              }
-              return response.json();
-            })
-            .then(data => {
-              const compraDetails = data.compra;
-              const currentRow = event.target.closest('tr'); // Encuentra la fila actual
-
-              const proveedor = currentRow.querySelector('input[name="proveedor[]"]');
-              proveedor.value = compraDetails.proveedor_id;
-
-              const marca = currentRow.querySelector('input[name="marca[]"]');
-              marca.value = compraDetails.marca_id;
-
-              const producto = currentRow.querySelector('input[name="producto[]"]');
-              producto.value = compraDetails.producto_id;
-
-              const aroma = currentRow.querySelector('input[name="aroma[]"]');
-              aroma.value = compraDetails.aroma_id;
-
-              const stock = currentRow.querySelector('input[name="stock[]"]');
-              stock.value = compraDetails.cantidad;
-
-              const precio = currentRow.querySelector('input[name="precio[]"]');
-              precio.value = compraDetails.precio_costo;
-
-              // Cálculos
-              document.getElementById('tbody').addEventListener('input', function (event) {
-                if (event.target && event.target.matches('input[name="cantidad[]"]')) {
-                  actualizarTotal();
-                }
-              });
-            })
-            .catch(error => console.error('Error fetching data:', error));
-        }
-      });
-    }
-    fetchURL();
-    function actualizarTotal() {
-      let totalGeneral = 0;
-      // Recorre todas las filas y suma los totales
-      document.querySelectorAll('#tbody tr').forEach(row => {
-        const cantidad = row.querySelector('input[name="cantidad[]"]').value;
-        const precio = row.querySelector('input[name="precio[]"]').value;
-        const subtotal = cantidad * precio;
-        totalGeneral += subtotal;
-      });
-      // Muestra el total general 
-      if (totalGeneral > 0) {
-        document.getElementById('total-compraLabel').hidden = false;
-        const compraDetailsInput = document.getElementById('total-compra');
-        compraDetailsInput.value = totalGeneral;
-        compraDetailsInput.hidden = false;
-      } else {
-        document.getElementById('total-compraLabel').hidden = true;
-        document.getElementById('total-compra').hidden = true;
-      }
-    }
-    function agregarFila() {
-      // Obtén la fila de plantilla
-      var templateRow = document.querySelector('.template-row');
-      // Clona la fila de plantilla
-      var newRow = templateRow.cloneNode(true);
-      // Limpia los valores de los inputs en la nueva fila
-      newRow.querySelectorAll('input').forEach(input => input.value = '');
-      newRow.querySelectorAll('select').forEach(select => select.selectedIndex = 0);
-
-      // Añade la nueva fila al tbody
-      document.getElementById('tbody').appendChild(newRow);
-    }
-
-    function eliminarFila(button) {
-      // Elimina la fila correspondiente al botón de eliminar
-      button.closest('tr').remove();
-    }
-  </script>
+  <script src="{{asset('js/ventas.js')}}"></script>
   @endsection
